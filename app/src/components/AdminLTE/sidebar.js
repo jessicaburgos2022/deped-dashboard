2dec629c735a3df382b7650d1be1c08e41eef2ff
Ralph <ralphescarilla@gmail.com>
2021-12-27 16:34:39 +0800

    Update

--- app/src/components/AdminLTE/sidebar.js
+++ app/src/components/AdminLTE/sidebar.js
@@ -33,7 +33,7 @@ export default () => {
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="index3.html" class="brand-link">
                 {/* <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> */}
-                <span class="brand-text font-weight-light">PRIME NCR PID</span>
+                <span class="brand-text font-weight-light">OP Dash</span>
             </a>
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
