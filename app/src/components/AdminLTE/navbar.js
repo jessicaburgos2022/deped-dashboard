f2c17357e30c483b0e2b43d85ff70a11a297989d
Ralph Escarilla <benjsurdilla@gmail.com>
2022-05-03 13:04:47 +0800

    Update

--- app/src/components/AdminLTE/navbar.js
+++ app/src/components/AdminLTE/navbar.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
-
-    return (
-        <React.Fragment>
-            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-                <ul class="navbar-nav">
-                    <li class="nav-item d-flex">
-                        <a class="nav-link px-3" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                        <span style={{padding:'8px 16px', fontSize:'1rem', fontWeight:'bold'}}>OP DASH</span>
-                    </li>
+  return (
+    <React.Fragment>
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <ul class="navbar-nav">
+          <li class="nav-item d-flex">
+            <a
+              class="nav-link px-3"
+              data-widget="pushmenu"
+              href="#"
+              role="button"
+            >
+              <i class="fas fa-bars"></i>
+            </a>
+            <span
+              style={{
+                padding: "8px 16px",
+                fontSize: "1rem",
+                fontWeight: "bold",
+              }}
+            >
+              OP DASH
+            </span>
+          </li>
+        </ul>
+        <div class="nav-collapse collapse">
+          <ul class="nav">
+            <li class="divider-vertical"></li>
+            <li>
+              <a href="#">
+                <i class="icon-home icon-white"></i> Home
+              </a>
+            </li>
+          </ul>
+          <div class="pull-right">
+            <ul class="nav pull-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  Welcome, User <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a href="/user/preferences">
+                      <i class="icon-cog"></i> Preferences
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/help/support">
+                      <i class="icon-envelope"></i> Contact Support
+                    </a>
+                  </li>
+                  <li class="divider"></li>
+                  <li>
+                    <a href="/auth/logout">
+                      <i class="icon-off"></i> Logout
+                    </a>
+                  </li>
                 </ul>
-            </nav>
-        </React.Fragment>
-    )
-}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
--- app/src/screens/OutputManagement/_components/Major/table.js
+++ app/src/screens/OutputManagement/_components/Major/table.js
@@ -1,140 +1,181 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
-import React, { useState } from 'react';
-import ViewOutput from './viewoutput';
-import ViewEdit from './editOutput';
-import { useSelector, useDispatch } from 'react-redux';
-import { searchMajorOutput, editOutputStatus } from '../../../../actions/outputActions';
-import Swal from 'sweetalert2';
-import CustomPagination from '../../../../components/CustomPagination';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import ViewOutput from "./viewoutput";
+import ViewEdit from "./editOutput";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  searchMajorOutput,
+  editOutputStatus,
+} from "../../../../actions/outputActions";
+import Swal from "sweetalert2";
+import CustomPagination from "../../../../components/CustomPagination";
 
 export default (data) => {
-    const userState = useSelector(state => state.user);
-    const dispatch = useDispatch();
-    const departmentId = userState.userInfo.acc[0].DepartmentId;
-    const { SearchResult } = data;
-    const [isViewOpen, setIsViewOpen] = useState(false);
-    const [isEditOpen, setIsEditOpen] = useState(false);
-    const [selectedRow, setSelectedRow] = useState({});
-    const perPage = 15;
-    const [currentPage, setCurrentPage] = useState(0);
-    const currentData = SearchResult
-        .slice(currentPage * perPage, currentPage * perPage + perPage);
+  const userState = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const departmentId = userState.userInfo.acc[0].DepartmentId;
+  const { SearchResult } = data;
+  const [isViewOpen, setIsViewOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({});
+  const perPage = 15;
+  const [currentPage, setCurrentPage] = useState(0);
+  const currentData = SearchResult.slice(
+    currentPage * perPage,
+    currentPage * perPage + perPage
+  );
 
-    const handleViewOpen = (data) => {
-        setSelectedRow(data);
-        setIsViewOpen(true)
-    }
-    const handleViewEdit = (data) => {
-        setSelectedRow(data);
-        setIsEditOpen(true)
-    }
-    const handleRefresh = () => {
-        dispatch(searchMajorOutput());
-    }
-    const handleEditOutputStatus = (oType, hId, sId) => {
-        Swal.fire({
-            title: "Confirmation",
-            text: "Do you want to approve?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-            showCancelButton: true,
-            confirmButtonText: "Yes",
-            denyButtonText: "No"
-        }).then(async (r) => {
-            if (r.isConfirmed) {
-                var ret = await dispatch(editOutputStatus({ outputtype: oType, headerid: hId, statusid: sId }))
-                Swal.fire(
-                    ret.result,
-                    ret.message,
-                    ret.result === "Success" ? "success" : "error"
-                );
-                handleRefresh();
-            }
-        });
-    }
-    return (
-        <TableContainer component={Paper} className="form">
-            {
-                isViewOpen && <ViewOutput data={selectedRow} open={isViewOpen} handleClose={() => setIsViewOpen(false)} />
-            }
-            {
-                isEditOpen && <ViewEdit data={selectedRow} open={isEditOpen} handleClose={() => setIsEditOpen(false)} handleRefresh={() => handleRefresh()} />
-            }
-            <Table aria-label="collapsible" className='table table-bordered'>
-                <TableHead className='thead'>
-                    <TableRow>
-                        <TableCell className="interface-table-header">
-                            Department
-                        </TableCell>
-                        <TableCell className="interface-table-header">
-                            Year
-                        </TableCell>
-                        <TableCell className="interface-table-header">
-                            KRA
-                        </TableCell>
-                        <TableCell className="interface-table-header">
-                            Program / Project
-                        </TableCell>
-                        <TableCell className="interface-table-header">
-                            Status
-                        </TableCell>
-                        <TableCell className="interface-table-header">Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        currentData && Array.isArray(currentData) && currentData.map(r => {
-                            return (
-                                <TableRow>
-                                    <TableCell component="td" className="interface-table-cell  text-center">
-                                        {r.Department}
-                                    </TableCell>
-                                    <TableCell component="td" className="interface-table-cell text-center">
-                                        {r.KRAYear}
-                                    </TableCell>
-                                    <TableCell component="td" className="interface-table-cell">
-                                        {r.KRAName}
-                                    </TableCell>
-                                    <TableCell component="td" className="interface-table-cell">
-                                        {r.Project}
-                                    </TableCell>
-                                    <TableCell component="td" className="interface-table-cell">
-                                        <span className="badge-pill badge-success">
-                                            {r.Status}
-                                            </span>
-                                    </TableCell>
-                                    <TableCell component="td" className="interface-table-cell">
-                                        <div style={{ display: 'flex', padding: 5 }}>
-                                            <Button variant="contained" className='btn btn-secondary' onClick={() => handleViewOpen(r)}>View</Button>
-                                            {
-                                                (parseInt(userState.userInfo.acc[0].RoleId) === 1 || (parseInt(departmentId) === parseInt(r.DepartmentId) && userState.userInfo.acc[0].RoleId === 3))
-                                                &&
-                                                <Button style={{marginLeft: 5}}  variant="contained" className='btn btn-secondary' onClick={() => handleViewEdit(r)}>Edit</Button>
-                                            }
-                                            {
-                                                (parseInt(userState.userInfo.acc[0].RoleId) === 1 || (parseInt(departmentId) === parseInt(r.DepartmentId) && userState.userInfo.acc[0].RoleId === 3 && r.StatusId === 1))
-                                                &&
-                                                <Button  style={{marginLeft: 5}} variant="contained" className='btn btn-secondary' onClick={() => handleEditOutputStatus(1, r.OutputMajorHeaderId, 2)} >Approve</Button>
-                                            }
-                                        </div>
-                                    </TableCell>
+  const handleViewOpen = (data) => {
+    setSelectedRow(data);
+    setIsViewOpen(true);
+  };
+  const handleViewEdit = (data) => {
+    setSelectedRow(data);
+    setIsEditOpen(true);
+  };
+  const handleRefresh = () => {
+    dispatch(searchMajorOutput());
+  };
+  const handleEditOutputStatus = (oType, hId, sId) => {
+    Swal.fire({
+      title: "Confirmation",
+      text: "Do you want to approve?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: "No",
+    }).then(async (r) => {
+      if (r.isConfirmed) {
+        var ret = await dispatch(
+          editOutputStatus({ outputtype: oType, headerid: hId, statusid: sId })
+        );
+        Swal.fire(
+          ret.result,
+          ret.message,
+          ret.result === "Success" ? "success" : "error"
+        );
+        handleRefresh();
+      }
+    });
+  };
+  return (
+    <TableContainer component={Paper} className="form">
+      {isViewOpen && (
+        <ViewOutput
+          data={selectedRow}
+          open={isViewOpen}
+          handleClose={() => setIsViewOpen(false)}
+        />
+      )}
+      {isEditOpen && (
+        <ViewEdit
+          data={selectedRow}
+          open={isEditOpen}
+          handleClose={() => setIsEditOpen(false)}
+          handleRefresh={() => handleRefresh()}
+        />
+      )}
+      <Table aria-label="collapsible" className="table table-bordered">
+        <TableHead className="thead">
+          <TableRow>
+            <TableCell className="interface-table-header">Department</TableCell>
+            <TableCell className="interface-table-header">Year</TableCell>
+            <TableCell className="interface-table-header">KRA</TableCell>
+            <TableCell className="interface-table-header">
+              Program / Project
+            </TableCell>
+            <TableCell className="interface-table-header">Status</TableCell>
+            <TableCell className="interface-table-header">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {currentData &&
+            Array.isArray(currentData) &&
+            currentData.map((r) => {
+              return (
+                <TableRow>
+                  <TableCell
+                    component="td"
+                    className="interface-table-cell  text-center"
+                  >
+                    {r.Department}
+                  </TableCell>
+                  <TableCell
+                    component="td"
+                    className="interface-table-cell text-center"
+                  >
+                    {r.KRAYear}
+                  </TableCell>
+                  <TableCell component="td" className="interface-table-cell">
+                    {r.KRAName}
+                  </TableCell>
+                  <TableCell component="td" className="interface-table-cell">
+                    {r.Project}
+                  </TableCell>
+                  <TableCell component="td" className="interface-table-cell">
+                    <span className={`badge-pill ${r.StatusId === 2 ? 'badge-success' : 'badge-warning' }`}>{r.Status}</span>
+                  </TableCell>
+                  <TableCell component="td" className="interface-table-cell">
+                    <div style={{ display: "flex", padding: 5 }}>
+                      <Button
+                        variant="contained"
+                        className="btn btn-secondary"
+                        onClick={() => handleViewOpen(r)}
+                      >
+                        View
+                      </Button>
+                      {(parseInt(userState.userInfo.acc[0].RoleId) === 1 ||
+                        (parseInt(departmentId) === parseInt(r.DepartmentId) &&
+                          userState.userInfo.acc[0].RoleId === 3)) && (
+                        <Button
+                          style={{ marginLeft: 5 }}
+                          variant="contained"
+                          className="btn btn-secondary"
+                          onClick={() => handleViewEdit(r)}
+                        >
+                          Edit
+                        </Button>
+                      )}
+                      {(parseInt(userState.userInfo.acc[0].RoleId) === 1 ||
+                        (parseInt(departmentId) === parseInt(r.DepartmentId) &&
+                          userState.userInfo.acc[0].RoleId === 3 &&
+                          r.StatusId === 1)) && (
+                        <Button
+                          style={{ marginLeft: 5 }}
+                          variant="contained"
+                          className="btn btn-secondary"
+                          onClick={() =>
+                            handleEditOutputStatus(1, r.OutputMajorHeaderId, 2)
+                          }
+                        >
+                          Approve
+                        </Button>
+                      )}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
 
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
-            
-            <CustomPagination perPage={perPage}
-            total={
-                SearchResult.length
-            }
-            paginate={(e, pageNumber) => setCurrentPage(pageNumber - 1)}
-            currentPage={currentPage + 1} >
-
-            </CustomPagination>
-            </TableContainer>
-    )
-}
+      <CustomPagination
+        perPage={perPage}
+        total={SearchResult.length}
+        paginate={(e, pageNumber) => setCurrentPage(pageNumber - 1)}
+        currentPage={currentPage + 1}
+      ></CustomPagination>
+    </TableContainer>
+  );
+};
