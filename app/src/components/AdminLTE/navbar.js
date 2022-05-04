7456d07a8257d35aa66c0f1ca9af110947a6403b
Ralph Escarilla <benjsurdilla@gmail.com>
2022-05-04 10:10:40 +0800

    Update

--- app/src/components/AdminLTE/navbar.js
+++ app/src/components/AdminLTE/navbar.js
@@ -1,68 +1,42 @@
 import React from "react";
+// import usercardheader from '../../media/user-dropdown-bg.jpg';
+import usercardheader from '../../media/user-dropdown-bg.jpg';
+import userimage from '../../media/userimage.jpg';
 
 export default () => {
   return (
     <React.Fragment>
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <ul class="navbar-nav">
-          <li class="nav-item d-flex">
-            <a
-              class="nav-link px-3"
-              data-widget="pushmenu"
-              href="#"
-              role="button"
-            >
-              <i class="fas fa-bars"></i>
+        <nav className="main-header navbar navbar-light navbar-expand bg-light">
+            <a className="toggle-button navbar-brand d-flex align-items-center" data-widget="pushmenu" href="#" role="button">
+                <i className="fas fa-bars"></i>
             </a>
-            <span
-              style={{
-                padding: "8px 16px",
-                fontSize: "1rem",
-                fontWeight: "bold",
-              }}
-            >
-              OP DASH
-            </span>
-          </li>
-        </ul>
-        <div class="nav-collapse collapse">
-          <ul class="nav">
-            <li class="divider-vertical"></li>
-            <li>
-              <a href="#">
-                <i class="icon-home icon-white"></i> Home
-              </a>
-            </li>
-          </ul>
-          <div class="pull-right">
-            <ul class="nav pull-right">
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  Welcome, User <b class="caret"></b>
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a href="/user/preferences">
-                      <i class="icon-cog"></i> Preferences
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/help/support">
-                      <i class="icon-envelope"></i> Contact Support
-                    </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                    <a href="/auth/logout">
-                      <i class="icon-off"></i> Logout
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+            <a className="navbar-brand fw-bold">OP DASH</a>
+              <ul className="nav navbar-nav ml-auto">
+                <li className="nav-item bottom-right position-relative">
+                  <a className="btn btn-userDropdown" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span className="mr-2">
+                      <small>Hi,&nbsp;</small>
+                      <b>Ralph</b>
+                    </span>
+                    <img src={userimage} alt="user image" className="user-image"></img>
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                    <div className="user-card-header notification-item-padding-x" style={{backgroundImage: `url(${usercardheader})`}}>
+                      <div className="user-card-avatar d-inline-flex">
+                        <img src={userimage} alt="user image" className="user-image"></img>
+                        <div className="user-card-name ml-3">
+                          <span>Ralph Christian Escarilla</span>
+                          <span>[QAD] Encoder</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="notification-custom">
+                      <a className="btn btn-danger border-0" href="javascript:;"> <i className="bx bx-log-out mr-2"></i>Sign Out</a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </nav>
     </React.Fragment>
   );
 };
